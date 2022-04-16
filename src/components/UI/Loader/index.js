2fa07bcb5e301e7cc61b6ef4331f81ddc0c3a89c
Artem <art.sled.ua@gmail.com>
2022-04-16 22:02:13 +0300

    init:
    add indicator reqests

---
+++ src/components/UI/Loader/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './index.scss';
+const Loader = () => {
+	return (
+		<div className="loadingio-spinner-rolling-0dtdsr1ll1v6">
+			<div className="ldio-4fhg6689rd5">
+				<div/>
+			</div>
+		</div>
+	)
+	
+
+}
+
+export default Loader;
